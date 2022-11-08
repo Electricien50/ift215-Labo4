8fbb7ac89d70823c595af444a315c93d7edf5834
valentin <valentin.biard@usherbrooke.ca>
2022-11-08 12:23:04 -0500

    update

---
+++ pro-gramme-base-lab3/client/scripts/produits.js
@@ -0,0 +1,16 @@
+$(function () {
+    console.log("coucou");
+
+
+    // $('selecteur').html()
+    // $('#list_items').append("<div></div>");
+    // $('<div></div>')
+    //     .addClass('card mb-4 text-center');
+    $.ajax({
+        url: "/produits",
+        success: function( result ) {
+        }
+
+    })
+
+});
