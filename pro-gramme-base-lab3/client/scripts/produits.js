b4668d1e3f4fcbd26eb7d72f3388c92fd15962b1
valentin <74057469+Electricien50@users.noreply.github.com>
2022-11-08 15:46:30 -0500

    almost working products
    Just need to implement products in list_items

--- pro-gramme-base-lab3/client/scripts/produits.js
+++ pro-gramme-base-lab3/client/scripts/produits.js
@@ -1,16 +1,72 @@
 $(function () {
     console.log("coucou");
+    corps = $("#corps-principal");
+    listItems = corps;
 
 
-    // $('selecteur').html()
-    // $('#list_items').append("<div></div>");
-    // $('<div></div>')
-    //     .addClass('card mb-4 text-center');
     $.ajax({
         url: "/produits",
         success: function( result ) {
-        }
+            for(i = 0; i<result.length; i++){
+                item = result[i];
 
+                dColMd3 = $('<div></div>')
+                    .addClass('col-md-3');
+
+                dCardMb4 = $('<div></div>')
+                    .addClass('card mb-4 rounded-3 shadow-sm');
+
+                dCardHead = $('<div></div>')
+                    .addClass('card-header py-3');
+
+                hMy0 = $('<h4></h4>')
+                    .addClass("my-0 fw-normal");
+                hMy0.text(item.nom);
+
+                dCardBody = $('<div></div>')
+                    .addClass('card-body');
+
+                hCardTitle = $('<h1></h1>')
+                    .addClass('card-title text-center');
+                hCardTitle.text(item.prix);
+
+                ulListUnstyled = $('<ul></ul>')
+                    .addClass("list-unstyled mt-3 mb-4")
+
+                li1 = $('<li></li>');
+                li1.text(item.qte_inventaire);
+
+                li2 = $('<li></li>');
+                li2.text(item.categorie.description);
+
+                pW100 = $('<p></p>')
+                    .addClass("w-100 display-6 text-center");
+
+                iBi = $('<i></i>')
+                    .addClass("bi bi-cart-plus");
+
+
+
+                dCardHead.append(hMy0);
+
+                ulListUnstyled.append(li1);
+                ulListUnstyled.append(li2);
+
+                pW100.append(iBi);
+
+                dCardBody.append(hCardTitle);
+                dCardBody.append(ulListUnstyled);
+                dCardBody.append(pW100);
+
+                dCardMb4.append(dCardHead);
+                dCardMb4.append(dCardBody);
+
+                dColMd3.append(dCardMb4);
+
+                corps.append(dColMd3)
+            }
+        }
     })
 
 });
+
